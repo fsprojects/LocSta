e974c02456d86864792fa1f573a7f745102754d2
SchlenkR <ronald.schlenker@binarygears.de>
2021-12-02 08:43:34 +0100

    test

---
+++ src/scratches/viewEngine.fsx
@@ -0,0 +1,38 @@
+
+#r "../LocSta/bin/Debug/netstandard2.0/LocSta.dll"
+open LocSta
+open LocSta.Lib.Gen
+
+let html elemName = [
+    $"<{elemName}>"
+
+    $"</{elemName}>"
+]
+
+let view =
+    feed {
+        let! state = Init 0
+        let! _ = count 0 1
+
+        yield 1, state + 1
+    }
+    |> Gen.toListn 10
+
+
+//let view =
+//    let (count, setCount) = React.useState(0)
+//    Html.div {
+//        Html.button {
+//            prop.style [ style.marginRight 5 ]
+//            prop.onClick (fun _ -> setCount(count + 1))
+//            prop.text "Increment"
+//        }
+
+//        Html.button {
+//            prop.style [ style.marginLeft 5 ]
+//            prop.onClick (fun _ -> setCount(count - 1))
+//            prop.text "Decrement"
+//        }
+
+//        Html.h1 count
+//    }
