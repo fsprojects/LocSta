8c0b4a71fa9316ac9dfd715140b3c886285256c7
SchlenkR <ronald.schlenker@binarygears.de>
2022-07-07 09:02:00 +0200

    scratch

--- src/scratches/fableTest/src/app.fs
+++ src/scratches/fableTest/src/app.fs
@@ -4,38 +4,72 @@ open LocSta
 open Browser
 open Browser.Types
 
-type HtmlElementEx = seq<string * string> -> string -> LoopGen<HTMLElement, HTMLElement>
+type ElementId = Id of int
 
-let createElem name = document.createElement name
+// TODO: Use reader instead of this hack
+[<AllowNullLiteral>]
+type App(appElement: HTMLElement, triggerUpdate: ElementId option -> HTMLElement) =
+    let mutable id = -1
+    let mutable currentTrigger = None
+    member _.createElement name =
+        id <- id + 1
+        document.createElement name, Id id
+    member _.run() =
+        let element = triggerUpdate None
+        do appElement.appendChild element |> ignore
+    member _.triggerUpdate id =
+        let element = triggerUpdate (Some id)
+        // TODO: element <> appElement.child -> throw
+        ()
 
-let elem name attributes content =
+let mutable app: App = null
+
+let toSeq (coll: NodeList) = seq { for i in 0..coll.length-1 do coll.Item i }
+
+let elem name attributes child =
     fun state ->
-        let element = state |> Option.defaultWith (fun () -> document.createElement name)
+        let elem,id = state |> Option.defaultWith (fun () -> app.createElement name)
         for aname,avalue in attributes do
-            let elemAttr = element.attributes.getNamedItem aname
+            let elemAttr = elem.attributes.getNamedItem aname
             if elemAttr.value <> avalue then
                 elemAttr.value <- avalue
-        if element.innerHTML <> content then
-            element.innerHTML <- content
-        Res.Loop.emit element element
+        if toSeq elem.childNodes |> Seq.contains child |> not then
+            elem.appendChild child |> ignore
+        Res.Loop.emit elem (elem,id)
     |> Gen.createLoop
 
-let div: HtmlElementEx = elem "div"
-let button: HtmlElementEx = elem "button"
-let p: HtmlElementEx = elem "p"
+let text content = document.createTextNode content
+let div attributes content = elem "div" attributes content
+let p attributes content = elem "p" attributes content
+let button (content: string) click = loop {
+    let! button = elem "button" [] (text content)
+    let button = button :?> HTMLButtonElement
+    match click with
+    | Some click ->
+        printfn "register click"
+        button.onclick <- click
+    | None -> 
+        printfn "no click"
+        button.onclick <- fun _ -> ()
+    button
+}
 
 let view = 
     loop {
         let! c = Gen.count 0 1
-        return! div [] $"Count = {c}"
+        let! button = button "Increment" (Some (fun args -> printfn "Clicked"))
+        return! div [] button
     }
     |> Gen.toEvaluable
 
 
 
-let app = document.querySelector("#app") :?> HTMLDivElement
-let initial = view.Evaluate().Value
-do app.appendChild initial |> ignore
+app <- App(
+    document.querySelector("#app") :?> HTMLDivElement,
+    fun _ -> view.Evaluate().Value
+)
 
-let evaluateButton = document.querySelector(".my-button") :?> HTMLButtonElement
-evaluateButton.onclick <- fun _ -> view.Evaluate()
+// let evaluateButton = document.querySelector(".my-button") :?> HTMLButtonElement
+// evaluateButton.onclick <- fun _ -> view.Evaluate()
+
+app.run()
